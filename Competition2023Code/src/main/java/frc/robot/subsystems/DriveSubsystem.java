046b929ace1e3504fa499b0240778a46f3142011
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-01-12 19:08:08 -0800

    Motors and speed

--- Competition2023Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ Competition2023Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,11 +4,33 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new DriveSubsystem. */
+
+  WPI_TalonSRX leftback = new WPI_TalonSRX(0);
+  WPI_TalonSRX rightback = new WPI_TalonSRX(1);
+  WPI_TalonSRX leftfront = new WPI_TalonSRX(2);
+  WPI_TalonSRX rightfront = new WPI_TalonSRX(3);
+
+  MotorControllerGroup left = new MotorControllerGroup(leftback, leftfront);
+  MotorControllerGroup right = new MotorControllerGroup(rightback, rightfront);
+
+  DifferentialDrive diffDrive = new DifferentialDrive(left, right);
+
+
   public DriveSubsystem() {}
+
+  public void drive(){
+
+    diffDrive.arcadeDrive(0.5, 0);
+
+  }
 
   @Override
   public void periodic() {
