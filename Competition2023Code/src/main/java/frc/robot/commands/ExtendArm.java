35b180f5362d9c13839b6ab76c0243207f022925
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-01-14 12:48:18 -0800

    aswdas

--- Competition2023Code/src/main/java/frc/robot/commands/ExtendArm.java
+++ Competition2023Code/src/main/java/frc/robot/commands/ExtendArm.java
@@ -4,15 +4,21 @@
 
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ArmSubsystem;
 
 public class ExtendArm extends CommandBase {
   /** Creates a new ExtendArm. */
 
+  ArmSubsystem m_ArmSubsystem;
+  DoubleSupplier speed;
 
-  public ExtendArm(ArmSubsystem m_ArmSubsystem, double time) {
-    
+
+  public ExtendArm(ArmSubsystem m_ArmSubsystem, DoubleSupplier speed) {
+    this.m_ArmSubsystem = m_ArmSubsystem;
+    this.speed = speed;  
 
   }
 
@@ -22,7 +28,9 @@ public class ExtendArm extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_ArmSubsystem.extendArm(speed.getAsDouble());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
