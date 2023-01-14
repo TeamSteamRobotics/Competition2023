ffc4191d40536b5db3da02be78305ea7588fb971
geg32 <10213143@students.usd497.org>
2023-01-14 10:58:09 -0600

    added armMotor and pushyMotor

--- Competition2023Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ Competition2023Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -8,10 +8,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
+
+  WPI_TalonSRX armMotor = new WPI_TalonSRX(deviceNumber; 5);
+
+  
+
   public ArmSubsystem() {}
+
+
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 }
+//among us will be real in: 3 minutes
