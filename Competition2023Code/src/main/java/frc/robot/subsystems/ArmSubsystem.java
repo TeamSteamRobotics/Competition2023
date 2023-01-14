ad1ca80a09a5302be0a8d95d046563da65cb485e
geg32 <10213143@students.usd497.org>
2023-01-14 11:00:05 -0600

    made the motors better

--- Competition2023Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ Competition2023Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,14 +4,18 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
 
-  WPI_TalonSRX armMotor = new WPI_TalonSRX(deviceNumber; 5);
+  WPI_TalonSRX armMotor = new WPI_TalonSRX(5);
 
-  
+  CANSparkMax pushyMotor1 = new CANSparkMax(7, null);
+  CANSparkMax pushyMotor2 = new CANSparkMax(8, null);
 
   public ArmSubsystem() {}
 
