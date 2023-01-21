158834c217635623506f3c089e83559a1cee5af3
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-01-21 12:24:05 -0800

    Merge

--- Competition2023Code/src/main/java/frc/robot/commands/EncoderDriveDistance.java
+++ Competition2023Code/src/main/java/frc/robot/commands/EncoderDriveDistance.java
@@ -24,11 +24,16 @@ public class EncoderDriveDistance extends PIDCommand {
         distanceMeters,
         // This uses the output
         output -> {
-          drive.drive(output, 0);
+          /*if(output > 0.2)
+            drive.drive(.2, 0);
+          else*/
+            drive.drive(-output / 2, 0);
           // Use the output here
         });
+      
     // Use addRequirements() here to declare subsystem dependencies.
     // Configure additional PID options by calling `getController` here.
+    getController().setTolerance(.1,1);
   }
 
   // Returns true when the command should end.
