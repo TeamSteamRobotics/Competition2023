755d0cbf2b343cbde73f1ffcbaf56fde5f661525
geg32 <10213143@students.usd497.org>
2023-01-24 20:42:27 -0600

    created AutoDriveBackwardsDock
    this command will drive backwards, and then, hopefully, if all is well, it will also dock. the 7 is a placeholder. figure it out later!

---
+++ Competition2023Code/src/main/java/frc/robot/commands/AutoDriveBackwardsDock.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class AutoDriveBackwardsDock extends CommandBase {
+  /** Creates a new AutoDriveBackwardsDock. */
+
+  EncoderDriveDistance grimbloFimblo;
+  DriveSubsystem m_driveSubsystem;
+  ArmSubsystem armSubsystem;
+  
+  public AutoDriveBackwardsDock(DriveSubsystem driveSubsystem, ArmSubsystem armSubsystem) {
+   
+    this.m_driveSubsystem = driveSubsystem;
+    this.armSubsystem = armSubsystem;
+    addRequirements(m_driveSubsystem, armSubsystem);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  grimbloFimblo = new EncoderDriveDistance(7, m_driveSubsystem);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  armSubsystem.stopAll();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
