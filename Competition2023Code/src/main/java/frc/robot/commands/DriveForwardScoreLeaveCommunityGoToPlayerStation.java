f02eb394cb6b3dc3e4b22e0f9353a49ee9a8bb7f
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-01-28 13:37:57 -0600

    robo hopefuly works

--- Competition2023Code/src/main/java/frc/robot/commands/DriveForwardScoreLeaveCommunityGoToPlayerStation.java
+++ Competition2023Code/src/main/java/frc/robot/commands/DriveForwardScoreLeaveCommunityGoToPlayerStation.java
@@ -5,30 +5,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class DriveForwardScoreLeaveCommunityGoToPlayerStation extends SequentialCommandGroup {
   /** Creates a new DriveForwardScoreLeaveCommunityGoToPlayerStation. */
-  public DriveForwardScoreLeaveCommunityGoToPlayerStation(DriveSubsystem drive, ArmSubsystem arm) {
+  public DriveForwardScoreLeaveCommunityGoToPlayerStation() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-    new EncoderDriveDistance(distanceMeters: 5, drive),
-    new ArmAnglePID(arm, angle: 90),
+      new EncoderDriveDistance(5, null),
+      // new ArmAnglePID(armSubsystem, 5),
+      // new Intake(armSubsystem)
+      new EncoderDriveDistance(-5, null),
+      new EncoderDriveDistance(1, null),
+      //                                            ^turn to 20 degrees but code mad for being dumb
+      new EncoderDriveDistance(10, null)
+      // turn -20 degrees toward player station
 
-    //Reverse Intake
-    //deploy intake
-    //uninake
-    //reset arm
-
-    new EncoderDriveDistance(distanceMeters: 5, drive),
-    //turn
-    new EncoderDriveDistance(distanceMeters: 5, drive),
-    //turn
-    new EncoderDriveDistance(distanceMeters: 5, drive),
-
-  );
+    );
   }
 }
