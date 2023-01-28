41b35a6525861b62d9b49fcf0a54719043a8c144
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-01-28 08:30:56 -0800

    updated drive forwards score

--- Competition2023Code/src/main/java/frc/robot/commands/DriveForwardAndScore.java
+++ Competition2023Code/src/main/java/frc/robot/commands/DriveForwardAndScore.java
@@ -5,15 +5,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class DriveForwardAndScore extends SequentialCommandGroup {
   /** Creates a new DriveForwardAndScore. */
-  public DriveForwardAndScore() {
+  public DriveForwardAndScore(DriveSubsystem drive, ArmSubsystem arm) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+
+      new EncoderDriveDistance(5, drive),
+      new ArmAnglePID(arm, 90),
+
+      //reverse intake
+
+      new EncoderDriveDistance(5, drive)
+
+
+    );
   }
 }
