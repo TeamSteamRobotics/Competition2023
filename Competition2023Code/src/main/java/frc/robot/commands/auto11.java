6d9983f2781434d49ccae7d152a74f8a9c209730
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-01-28 15:30:12 -0600

    renamed auto 11 to auto 11

--- Competition2023Code/src/main/java/frc/robot/commands/auto11.java
+++ Competition2023Code/src/main/java/frc/robot/commands/auto11.java
@@ -11,9 +11,9 @@ import frc.robot.subsystems.DriveSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class DriveForwardScoreLeaveCommunityGoToPlayerStation extends SequentialCommandGroup {
+public class auto11 extends SequentialCommandGroup {
   /** Creates a new DriveForwardScoreLeaveCommunityGoToPlayerStation. */
-  public DriveForwardScoreLeaveCommunityGoToPlayerStation(DriveSubsystem drive, ArmSubsystem arm) {
+  public auto11(DriveSubsystem drive, ArmSubsystem arm) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
