368e8cb42595c4134d811609bb6edd55a859baba
geg32 <10213143@students.usd497.org>
2023-01-28 15:31:12 -0600

    renamed AutoDriveBackwardsDockAndEngage and also, in addition to this, AutoDriveForwardsScoreDriveBackwardsDock
    please help me

--- Competition2023Code/src/main/java/frc/robot/commands/Auto3.java
+++ Competition2023Code/src/main/java/frc/robot/commands/Auto3.java
@@ -13,9 +13,9 @@ import frc.robot.subsystems.DriveSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoDriveBackwardsDockAndEngage extends SequentialCommandGroup {
+public class Auto3 extends SequentialCommandGroup {
   /** Creates a new AutoDriveBackwardsDockAndEngage. */
-  public AutoDriveBackwardsDockAndEngage(DriveSubsystem drive, ArmSubsystem arm) {
+  public Auto3(DriveSubsystem drive, ArmSubsystem arm) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
