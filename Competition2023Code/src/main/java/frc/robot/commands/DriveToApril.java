c8bfc6dbf7b0ddd3357d50c125dd26814cdf8199
LogEman <thelogankessler@gmail.com>
2023-02-03 11:53:30 -0600

    Revert "Merge branch 'vision-subsystem' into develop"
    This reverts commit 19e82f5ab37c8ba745924d4b73566eac60a0aca2, reversing
    changes made to 2b78d607f237a14f329b8c21376b4806e2c27055.

--- Competition2023Code/src/main/java/frc/robot/commands/DriveToApril.java
+++ Competition2023Code/src/main/java/frc/robot/commands/DriveToApril.java
@@ -1,58 +1,50 @@
 
 package frc.robot.commands;
 
-import java.util.Set;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants.EncoderDriveDistanceConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.AprilVisionSubsystem;
 
-public class DriveToApril extends CommandBase {
-  
-  private AprilVisionSubsystem m_aprilVisionSubsystem;
-  private DriveSubsystem m_driveSubsystem;
-  public float targetDistance;
-  private boolean aprilTagVisible;
-  public float robotSpeed;
-  private float currentDistance;
-  private boolean commandFinished;
 
-  public DriveToApril(AprilVisionSubsystem vision, DriveSubsystem drive) {
-    addRequirements(vision, drive);
-    m_aprilVisionSubsystem = vision;
-    m_driveSubsystem = drive;
-    
-    robotSpeed = 0.5f;
-    targetDistance = 2.0f;
 
-    currentDistance = 0.0f;
 
-    commandFinished = false;
-  }
-  @Override
-  public void execute(){
-    currentDistance = m_aprilVisionSubsystem.getCoordinates().z;
-    aprilTagVisible = m_aprilVisionSubsystem.getCoordinates().aprilTagVisible;
-    if(aprilTagVisible){
-      if(currentDistance > targetDistance){
-      System.out.println(currentDistance);
-      m_driveSubsystem.drive(robotSpeed, 0);
-      commandFinished = false;
-      }else{
-        commandFinished = true;
-      }
-    }else{
-      commandFinished = false;
-      System.out.println("NO APRILTAGS VISIBLE");
+public class DriveToApril {
+  /** Creates a new EncoderDriveDistance. */
+  public DriveToApril(DriveSubsystem drive, AprilVisionSubsystem vision) {
+    while(vision.getCoordinates().x > 0.5){
+      drive.drive(.2, 0);
     }
+    /*super(
+        // The controller that the command will use
+        new PIDController(EncoderDriveDistanceConstants.kP, EncoderDriveDistanceConstants.kI, EncoderDriveDistanceConstants.kD),
+        // This should return the measurement
+        () -> drive.getEncoderDistanceMeters(),
+        // This should return the setpoint (can also be a constant)
+        vision.getCoordinates().z-1,
+        // This uses the output
+        output -> {
+          /*if(output > 0.2)
+            drive.drive(.2, 0);
+          else
+
+            drive.drive(-output / 2, 0);
+          // Use the output here
+    });
+    */
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+    System.out.println(vision.getCoordinates().z);
   }
-  @Override
-  public void end(boolean interrupted) {
-    m_driveSubsystem.stop();
-    commandFinished = true;
-  }
-  @Override
+
+ 
+
+  // Returns true when the command should end.
+ /*  @Override
   public boolean isFinished() {
-    return commandFinished;
-  }
+    return getController().atSetpoint();
+  }*/
 }
+
