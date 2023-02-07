0195185ca6ab9e47be929d5af49cd03b16886fff
Antelope10 <86484092+Antelope10@users.noreply.github.com>
2023-02-06 19:08:32 -0600

    tracked armtoggle

---
+++ Competition2023Code/src/main/java/frc/robot/commands/ArmTogglePID.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.security.AlgorithmConstraints;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.subsystems.ArmSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+
+
+
+public class ArmTogglePID extends PIDCommand {
+  /** Creates a new ArmAnglePID. */
+  
+  public ArmTogglePID(ArmSubsystem arm, Boolean increase) {
+    super(
+        // The controller that the command will use
+        new PIDController(ArmConstants.angle_kP, ArmConstants.angle_kI, ArmConstants.angle_kD),
+        // This should return the measurement
+        () -> arm.getArmAngleDegrees(), //also increments index
+        // This should return the setpoint (can also be a constant)
+        ArmConstants.positions[arm.getIndex()],
+        // This uses the output
+        output -> {
+          arm.getArmAngleDegrees();
+          arm.angleArm(output);
+          // Use the output here
+        });
+    addRequirements(arm);
+    this.getController().setTolerance(ArmConstants.anglePIDTolerance);
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return this.getController().atSetpoint();
+  }
+}
