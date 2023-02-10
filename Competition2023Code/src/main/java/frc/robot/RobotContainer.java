31d41c125f5b24fce88c7c4cc42230d8fcec0186
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-02-09 20:26:09 -0600

    Update RobotContainer.java

--- Competition2023Code/src/main/java/frc/robot/RobotContainer.java
+++ Competition2023Code/src/main/java/frc/robot/RobotContainer.java
@@ -82,7 +82,7 @@ public class RobotContainer {
   private final Trigger extendArm = new JoystickButton(joystick, 9);
   private final Trigger unextendArm = new JoystickButton(joystick, 10);
   private final Trigger manualArm = new JoystickButton(joystick, 2);
-  
+ 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
