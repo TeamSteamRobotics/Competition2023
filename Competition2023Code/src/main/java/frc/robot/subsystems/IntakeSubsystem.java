fabb5ad52c21367c5b7c271f3512ac920480fdb8
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-02-11 14:09:05 -0600

    saved it again to be sure

--- Competition2023Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ Competition2023Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,10 +5,36 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.MotorIDConstants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
+  private CANSparkMax intakeMotor = new CANSparkMax(MotorIDConstants.intakeMotor, MotorType.kBrushless);
+  private Solenoid intakeSolenoid =  new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+  
   public IntakeSubsystem() {}
+
+  public void intake(double speed){
+    intakeMotor.set(speed);
+  }
+
+  public void stopIntake(){
+    intakeMotor.set(0);
+  }
+
+  public void retractIntake(){
+    intakeSolenoid.set(false);
+  }
+
+  public void deployIntake(){
+    intakeSolenoid.set(true);
+  }
 
   @Override
   public void periodic() {
