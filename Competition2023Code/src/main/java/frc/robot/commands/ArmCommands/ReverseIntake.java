6f00fdffb4200053606426097424e79595e7b2b8
BradyChild <10201349@students.usd497.org>
2023-02-11 09:48:19 -0600

    Created Reverse Intake method
    Intake value set inverse to un- intake.

---
+++ Competition2023Code/src/main/java/frc/robot/commands/ArmCommands/ReverseIntake.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ArmCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class ReverseIntake extends CommandBase {
+  /** Creates a new ReverseIntake. */
+  ArmSubsystem m_ArmSubsystem;
+  public ReverseIntake(ArmSubsystem m_ArmSubsystem) {
+    this.m_ArmSubsystem = m_ArmSubsystem;
+    addRequirements(m_ArmSubsystem);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_ArmSubsystem.intake(ArmConstants.intakeSpeed * -1);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_ArmSubsystem.stopIntake();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
