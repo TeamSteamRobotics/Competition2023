8d70e0223fb693b6cb6ab195c7866f59edd553d5
LogEman <thelogankessler@gmail.com>
2023-02-15 11:38:48 -0600

    Rewrote DriveToApril (againnnnnn)
    - Added Asynchronous Logging(logs to the console without blocking the code, probably pointless but still cool)
    - The movement of the robot first prioritizes aligning to the center until it is a certain distance away from the Apriltag where it will then start going forward.

--- Competition2023Code/src/main/java/frc/robot/commands/DriveToApril.java
+++ Competition2023Code/src/main/java/frc/robot/commands/DriveToApril.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.AprilVisionSubsystem;
 
+import java.util.concurrent.CompletableFuture;
+
 public class DriveToApril extends CommandBase {
   
   private AprilVisionSubsystem m_aprilVisionSubsystem;
@@ -20,6 +22,11 @@ public class DriveToApril extends CommandBase {
   private boolean commandFinished;
   private boolean inverted;
 
+  enum Step {
+    MAXTURN,
+    FORWARD
+  }
+  Step currentStep;
   public DriveToApril(AprilVisionSubsystem vision, DriveSubsystem drive, float speed, float distance, boolean invertDistance) {
     
     addRequirements(vision, drive);
@@ -36,57 +43,65 @@ public class DriveToApril extends CommandBase {
     commandFinished = false;
   }
   @Override
+  public void initialize(){
+    currentStep = Step.MAXTURN;
+  }
+  @Override
   public void execute(){
     currentDistanceRotation = m_aprilVisionSubsystem.getCoordinates(6).x;
     currentDistance = m_aprilVisionSubsystem.getCoordinates(6).z;
     aprilTagVisible = m_aprilVisionSubsystem.getCoordinates(6).aprilTagVisible;
+    asyncLog();
 
-    forwardTarget();
+    switch(currentStep){
+      case MAXTURN:
+        maxTurn();
+      break;
+      case FORWARD:
+        forward();
+      break;
+
+    }
   }
-  private void forwardTarget(){
+public void asyncLog(){
+    CompletableFuture<Void> future = CompletableFuture.runAsync(() ->{
+      System.out.println("Current X/Rotation: " + m_aprilVisionSubsystem.getCoordinates(6).x);
+      System.out.println("Current Z/Distance: " + m_aprilVisionSubsystem.getCoordinates(6).z);
+    });
+  }
+
+  private void maxTurn(){
+    float lastSeenRotation = 0;
+    float lastSeenDistance = 0;
+    if(aprilTagVisible && currentDistance > 2.0f){
+      lastSeenRotation = currentDistanceRotation;
+      lastSeenDistance = currentDistance;
+      System.out.println();
+      m_driveSubsystem.drive(-robotSpeed, 0.35 * -Math.signum(currentDistanceRotation));
+    }else if(!aprilTagVisible && lastSeenDistance > targetDistance){
+      m_driveSubsystem.drive(0, 0.25 * Math.signum(lastSeenRotation));
+    }else{
+      currentStep = Step.FORWARD;
+    }
+  }
+  private void forward(){
     if(aprilTagVisible){
       System.out.println("Current Distance: " + currentDistance);
       System.out.println("Current Rotation: " + currentDistanceRotation);
       if(currentDistance > targetDistance && !inverted){
-        if(currentDistanceRotation > 0.45){
-          m_driveSubsystem.drive(-robotSpeed, 0.35f);
-        }else if(currentDistanceRotation < -0.45){
-          m_driveSubsystem.drive(-robotSpeed, -0.35f);
+        if(Math.abs(currentDistanceRotation) > 0.45){
+          m_driveSubsystem.drive(0, Math.signum(currentDistanceRotation) * 0.35f);
         }else{
           m_driveSubsystem.drive(-robotSpeed, 0.0f);
         }
       commandFinished = false;
-      }else if(currentDistance < targetDistance && inverted){
-        System.out.println("Current Distance: " + currentDistance);
-        m_driveSubsystem.drive(robotSpeed, 0);
-        commandFinished = false;
-      }else{
-        commandFinished = false;
+     }else if(currentDistance <= targetDistance){
+      commandFinished = true;
       }
-    }else{
-      if(!aprilTagVisible){
-      commandFinished = false;
-      }
-    }
-    //Thanks chatgpt for saving my ass like forty times
-  }
- /*  private void centerTarget(boolean finalCenter){
-    if(aprilTagVisible && currentDistance > targetDistance - 0.5){
-      System.out.println("Current Rotation: " + currentDistanceRotation);
-      if(currentDistanceRotation > 1.5){
-        m_driveSubsystem.drive(0, 0.5f);
-        commandFinished = false;
-      }else if(currentDistanceRotation < 1){
-        m_driveSubsystem.drive(0, -0.5f);
-        commandFinished = false;
-      }else if(!finalCenter){
-       forwardTarget();
-      }else{
-        forwardTarget();
-      }
+    }else if(!aprilTagVisible){
+      commandFinished = true;
     }
   }
-  */
   @Override
   public void end(boolean interrupted) {
     m_driveSubsystem.stop();
