1d859244c53b28c6ecb00cade6908eb6b7b26ca3
Antelope10 <86484092+Antelope10@users.noreply.github.com>
2023-02-24 08:43:28 -0600

    k

--- Competition2023Code/src/main/java/frc/robot/commands/Autos/Auto2.java
+++ Competition2023Code/src/main/java/frc/robot/commands/Autos/Auto2.java
@@ -5,8 +5,14 @@
 package frc.robot.commands.Autos;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPRamseteCommand;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
 import frc.robot.commands.DriveCommands.EncoderDriveDistance;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
@@ -16,12 +22,37 @@ import frc.robot.subsystems.DriveSubsystem;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class Auto2 extends SequentialCommandGroup {
   /** Creates a new Auto2. */
-  public Auto2(DriveSubsystem driveSubsystem, ArmSubsystem armSubsystem, PathPlannerTrajectory path, Boolean isFirstPath) {
+  
+  //test values
+  double KS = .5;
+  double KV = .5;
+  double KA = .5; 
+
+  public Auto2(DriveSubsystem driveSubsystem, ArmSubsystem armSubsystem, PathPlannerTrajectory traj, Boolean isFirstPath) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      
-    
+      new SequentialCommandGroup(
+          new InstantCommand(() -> {
+            // Reset odometry for the first path you run during auto
+            if(isFirstPath){
+                this.resetOdometry(traj.getInitialPose());
+            }
+          }),
+          new PPRamseteCommand(
+              traj, 
+              this::getPose, // Pose supplier
+              new RamseteController(),
+              new SimpleMotorFeedforward(KS,KV,KA),
+              this.kinematics, // DifferentialDriveKinematics
+              this::getWheelSpeeds, // DifferentialDriveWheelSpeeds supplier
+              new PIDController(0, 0, 0), // Left controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+              new PIDController(0, 0, 0), // Right controller (usually the same values as left controller)
+              this::outputVolts, // Voltage biconsumer
+              true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+              this // Requires this drive subsystem
+          )
+      )
     );
   }
 }
