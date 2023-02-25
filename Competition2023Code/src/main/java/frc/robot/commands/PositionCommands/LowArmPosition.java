504d89b1da2d028eaddcf53a18ad43190e208cc2
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-02-25 13:49:40 -0600

    fixed low and middle arm position

--- Competition2023Code/src/main/java/frc/robot/commands/PositionCommands/LowArmPosition.java
+++ Competition2023Code/src/main/java/frc/robot/commands/PositionCommands/LowArmPosition.java
@@ -5,12 +5,14 @@
 package frc.robot.commands.PositionCommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.commands.ArmCommands.ArmAnglePID;
+import frc.robot.commands.ArmCommands.DeployIntake;
+import frc.robot.commands.ArmCommands.ExtendArm;
 import frc.robot.commands.ArmCommands.ExtendArmPID;
-import frc.robot.commands.ArmCommands.RetractIntake;
 import frc.robot.subsystems.ArmExtensionSubsystem;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.PneumaticsSubsystem;
@@ -18,20 +20,25 @@ import frc.robot.subsystems.PneumaticsSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class MiddleArmPosition extends ParallelCommandGroup {
-  /** Creates a new MiddleArmPosition. */
-  public MiddleArmPosition(ArmExtensionSubsystem m_armExtensionSubsystem, PneumaticsSubsystem m_pneumaticsSubsystem, ArmSubsystem m_armSubsystem) {
+public class LowArmPosition extends ParallelCommandGroup {
+ 
+  /** Creates a new LowArmPosition. */
+  public LowArmPosition(ArmExtensionSubsystem m_armExtensionSubsystem, PneumaticsSubsystem m_pneumaticsSubsystem, ArmSubsystem m_armSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      
-      new ArmAnglePID(m_armSubsystem, ArmConstants.middlePosition),
-       new SequentialCommandGroup(
-        new WaitCommand(0.5),
-        new RetractIntake(m_pneumaticsSubsystem)), 
+
       new SequentialCommandGroup(
         new WaitCommand(1),
-        new ExtendArmPID(m_armExtensionSubsystem, ArmConstants.middlePositionLength))
-        );
+        new ArmAnglePID(m_armSubsystem, ArmConstants.lowPosition)),
+      new SequentialCommandGroup(
+        new ParallelRaceGroup(
+          new ExtendArm(m_armExtensionSubsystem, -0.2), 
+          new WaitCommand(1)),
+        new WaitCommand(1),
+        new DeployIntake(m_pneumaticsSubsystem),
+        new WaitCommand(0.5),
+        new ExtendArmPID(m_armExtensionSubsystem, ArmConstants.lowPositionLength))
+    );
   }
 }
