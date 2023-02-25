37ad9f433dfb2dbc9079cd8abe29913f4510afed
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-02-25 13:56:01 -0600

    Update HighArmPosition.java

--- Competition2023Code/src/main/java/frc/robot/commands/PositionCommands/HighArmPosition.java
+++ Competition2023Code/src/main/java/frc/robot/commands/PositionCommands/HighArmPosition.java
@@ -5,15 +5,34 @@
 package frc.robot.commands.PositionCommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.commands.ArmCommands.ArmAnglePID;
+import frc.robot.commands.ArmCommands.ExtendArmPID;
+import frc.robot.commands.ArmCommands.RetractIntake;
+import frc.robot.subsystems.ArmExtensionSubsystem;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.PneumaticsSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class HighArmPosition extends ParallelCommandGroup {
   /** Creates a new HighArmPosition. */
-  public HighArmPosition() {
+  public HighArmPosition(ArmExtensionSubsystem m_armExtensionSubsystem, PneumaticsSubsystem m_pneumaticsSubsystem, ArmSubsystem m_armSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+      new ParallelCommandGroup(
+        new ArmAnglePID(m_armSubsystem, ArmConstants.highPosition),
+        new SequentialCommandGroup(
+        new WaitCommand(0.5),
+        new RetractIntake(m_pneumaticsSubsystem)),
+      new SequentialCommandGroup(
+        new WaitCommand(1),
+        new ExtendArmPID(m_armExtensionSubsystem, ArmConstants.highPositionLength)))
+
+    );
   }
 }
