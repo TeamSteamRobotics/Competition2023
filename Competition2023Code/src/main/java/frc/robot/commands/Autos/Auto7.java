ba215b937c0aee2b87ca415097f487740cdfa15e
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2023-03-11 14:30:44 -0600

    Update Auto7.java
    new Auto7 command is ready for testing

--- Competition2023Code/src/main/java/frc/robot/commands/Autos/Auto7.java
+++ Competition2023Code/src/main/java/frc/robot/commands/Autos/Auto7.java
@@ -11,7 +11,10 @@ import frc.robot.commands.ArmCommands.Intake;
 import frc.robot.commands.ArmCommands.ReverseIntake;
 import frc.robot.commands.ArmCommands.PositionCommands.HighArmPosition;
 import frc.robot.commands.ArmCommands.PositionCommands.LowArmPosition;
+import frc.robot.commands.ArmCommands.PositionCommands.MiddleArmPosition;
+import frc.robot.commands.DriveCommands.Drive;
 import frc.robot.commands.DriveCommands.EncoderDriveDistance;
+import frc.robot.commands.DriveCommands.GyroTurn;
 import frc.robot.subsystems.ArmExtensionSubsystem;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
@@ -24,14 +27,30 @@ import frc.robot.subsystems.PneumaticsSubsystem;
 public class Auto7 extends SequentialCommandGroup {
   /** Creates a new Auto7. */
   //Drives forwards, scores, drives back (leaves community), picks up piece
-  public Auto7(DriveSubsystem drive, ArmSubsystem arm, PneumaticsSubsystem deployIntake, ArmExtensionSubsystem armExtension, IntakeSubsystem intake) {
+  public Auto7(DriveSubsystem drive, ArmSubsystem arm, PneumaticsSubsystem deployIntake, ArmExtensionSubsystem armExtension, IntakeSubsystem intake, PneumaticsSubsystem pneumatics, ArmSubsystem armRotation) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-
-    new EncoderDriveDistance(.3, drive),
+      new ReverseIntake(intake).raceWith(new WaitCommand(1)),
+      new ParallelCommandGroup(
+        new MiddleArmPosition(armExtension, pneumatics, armRotation),
+        new SequentialCommandGroup(
+          new WaitCommand(1),
+          new Intake(intake).raceWith(new WaitCommand(1))
+        )
+      ).raceWith(new WaitCommand(4)),
+      
+      new Drive(drive, () -> 0.5, () -> 0).raceWith(new WaitCommand(7)),
+      new GyroTurn(drive, 180).raceWith(new WaitCommand(2)),
+      new ParallelCommandGroup(
+        new LowArmPosition(armExtension, pneumatics, arm),
+        new Drive(drive, () -> 0.5, () -> 0),
+        new ReverseIntake(intake)
+        ).raceWith(new WaitCommand(2))
+      );
+    /* new EncoderDriveDistance(.3, drive),
     new ParallelCommandGroup(
-      new HighArmPosition(armExtension, deployIntake, arm),
+     new HighArmPosition(armExtension, deployIntake, arm),
       new SequentialCommandGroup(
         new WaitCommand(0.5),
         new ReverseIntake(intake))),
@@ -49,7 +68,7 @@ public class Auto7 extends SequentialCommandGroup {
 
     //open intake
     //rotate arm and drive back
-    //align with piece and close intake
-    );
+    //align with piece and close intake*/
+    
   }
 }
